838decfa432480570c19a1648cc554dc08af7fb6
Putrifisichella <2210631170096@student.unsika.ac.id>
2024-09-06 12:54:07 +0700

    progres praktikum

---
+++ Pertemuan3/sistem-toko/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/hello', function () {
+    return 'Hello, World!';
+});
+
+Route::get('/user/{id}', function($id){
+    return "User ID: ".$id;
+});
+
+Route::get('/user/{name?}', function($name='Guest'){
+    return "Hello, ".$name;
+});
+
+Route::get('/profile', function(){
+    return 'This is the profile page.';
+});
+
+Route::get('/redirect-to-profile',function(){
+    return redirect()->route('profile');
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/dashboard', function(){
+        return'Admin Dashboard';
+    });
+
+    Route::get('/profile',function(){
+        return 'Admin Profile';
+    });
+});
+
+Route::get('/dashboard', function(){
+    return 'Welcome to your dashboard!';
+})->middleware('auth');
+
+Route::resource('posts','PostController');
