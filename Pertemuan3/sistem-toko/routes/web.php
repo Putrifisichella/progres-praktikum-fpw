d48ec09181dd334671554e5de033be4a7d74d5cc
Putrifisichella <2210631170096@student.unsika.ac.id>
2024-10-22 13:35:49 +0700

    Kode program sampai bab 5

--- Pertemuan3/sistem-toko/routes/web.php
+++ Pertemuan3/sistem-toko/routes/web.php
@@ -1,39 +1,43 @@
 <?php
 
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return 'Hello, World!';
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/user/{id}', function($id){
-    return "User ID: ".$id;
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified','RoleChechk:admin'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/user/{name?}', function($name='Guest'){
-    return "Hello, ".$name;
-});
+require __DIR__.'/auth.php';
 
-Route::get('/profile', function(){
-    return 'This is the profile page.';
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('dashboard');
 
-Route::get('/redirect-to-profile',function(){
-    return redirect()->route('profile');
-});
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product/create', [ProductController::class, 'create'])->name('product-create');
+Route::post('/product', [ProductController::class, 'store'])->name('product-store');
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/product/{id}/edit', [ProductController::class, 'edit']);
+Route::put('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destory']);
 
-Route::prefix('admin')->group(function(){
-    Route::get('/dashboard', function(){
-        return'Admin Dashboard';
-    });
 
-    Route::get('/profile',function(){
-        return 'Admin Profile';
-    });
-});
-
-Route::get('/dashboard', function(){
-    return 'Welcome to your dashboard!';
-})->middleware('auth');
-
-Route::resource('posts','PostController');
+Route::get('/supplier', [SupplierController::class, 'index']);
+Route::get('/supplier/create', [SupplierController::class, 'create'])->name("supplier-create");
+Route::post('/supplier', [SupplierController::class, 'store'])->name("supplier-store");
+Route::get('/supplier/{id}', [SupplierController::class, 'show']);
+Route::get('/supplier/{id}/edit', [SupplierController::class, 'edit']);
+Route::put('/supplier/{id}', [SupplierController::class, 'update']);
+Route::delete('/supplier/{id}', [SupplierController::class, 'destory']);
